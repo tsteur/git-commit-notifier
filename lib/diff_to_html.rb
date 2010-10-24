a6a4ab1deea41f5da983035f973039dea8236960
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2010-10-24 13:52:02 +0400

    Minor code style changes

--- lib/diff_to_html.rb
+++ lib/diff_to_html.rb
@@ -288,7 +288,7 @@ class DiffToHtml
   end
 
   def message_array_as_html(message)
-    message_map(message.collect { |m| CGI.escapeHTML(m)}.join("<br />"))
+    message_map(message.collect { |m| CGI.escapeHTML(m) }.join('<br />'))
   end
 
   def author_name_and_email(info)
@@ -346,7 +346,7 @@ class DiffToHtml
       commits = []
     else
       log = Git.log(rev1, rev2)
-      commits = log.scan(/^commit\s([a-f0-9]+)/).map { |match| match[0] }
+      commits = log.scan(/^commit\s([a-f0-9]+)/).map(&:first)
     end
 
     commits = check_handled_commits(commits, branch)
@@ -397,14 +397,14 @@ class DiffToHtml
   end
 
   def message_map(message)
-    if @config.include?('message_integration') && @config['message_integration'].respond_to?(:each_pair)
+    if @config['message_integration'].respond_to?(:each_pair)
       @config['message_integration'].each_pair do |pm, url|
         pm_def = DiffToHtml::INTEGRATION_MAP[pm.to_sym] or next
         replace_with = pm_def[:replace_with].gsub('#{url}', url)
         message_replace!(message, pm_def[:search_for], replace_with)
       end
     end
-    if @config.include?('message_map') && @config['message_map'].respond_to?(:each_pair)
+    if @config['message_map'].respond_to?(:each_pair)
       @config['message_map'].each_pair do |search_for, replace_with|
         message_replace!(message, Regexp.new(search_for), replace_with)
       end
