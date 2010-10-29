ea6adf35a87525f25c456f3ca3af639cdc043d54
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2010-10-29 14:13:46 +0400

    Specs for Git class

--- lib/git.rb
+++ lib/git.rb
@@ -1,41 +1,44 @@
 class Git
   class << self
+    def from_shell(cmd)
+      `#{cmd}`
+    end
+
     def show(rev)
-      `git show #{rev.strip} -w`
+      from_shell("git show #{rev.strip} -w")
     end
 
     def log(rev1, rev2)
-      `git log #{rev1}..#{rev2}`.strip
+      from_shell("git log #{rev1}..#{rev2}").strip
     end
 
     def branch_commits(treeish)
       args = Git.branch_heads - [ Git.branch_head(treeish) ]
       args.map! { |tree| "^#{tree}" }
       args << treeish
-      lines = `git rev-list #{args.join(' ')}`
+      lines = from_shell("git rev-list #{args.join(' ')}")
       lines = lines.lines if lines.respond_to?(:lines)
       lines.to_a.map { |commit| commit.chomp }
     end
 
     def branch_heads
-      lines = `git rev-parse --branches`
+      lines = from_shell("git rev-parse --branches")
       lines = lines.lines if lines.respond_to?(:lines)
       lines.to_a.map { |head| head.chomp }
     end
 
     def branch_head(treeish)
-      `git rev-parse #{treeish}`.strip
+      from_shell("git rev-parse #{treeish}").strip
     end
 
     def repo_name
-      git_prefix = `git config hooks.emailprefix`.strip
+      git_prefix = from_shell("git config hooks.emailprefix").strip
       return git_prefix unless git_prefix.empty?
-      dir_name = `pwd`.chomp.split("/").last.gsub(/\.git$/, '')
-      return "#{dir_name}"
+      Dir.pwd.split("/").last.sub(/\.git$/, '')
     end
 
     def mailing_list_address
-      `git config hooks.mailinglist`.strip
+      from_shell("git config hooks.mailinglist").strip
     end
   end
 end
---
+++ spec/lib/git_spec.rb
@@ -0,0 +1,71 @@
+require File.expand_path('../../spec_helper', __FILE__)
+require 'git'
+
+describe Git do
+  SAMPLE_REV = '51b986619d88f7ba98be7d271188785cbbb541a0'.freeze
+
+  describe :from_shell do
+    it "should be backtick" do
+      Git.from_shell('pwd').should == `pwd`
+    end
+  end
+
+  describe :show do
+    it "should get data from shell: git show" do
+      expected = 'some data from git show'
+      mock(Git).from_shell("git show #{SAMPLE_REV} -w") { expected }
+      Git.show(SAMPLE_REV).should == expected
+    end
+
+    it "should strip given revision" do
+      mock(Git).from_shell("git show #{SAMPLE_REV} -w")
+      Git.show("#{SAMPLE_REV}\n")
+    end
+  end
+
+  describe :branch_heads do
+    before(:each) do
+      mock(Git).from_shell("git rev-parse --branches") { "some\npopular\ntext\n" }
+    end
+
+    it "should get branch heads from shell" do
+      lambda { Git.branch_heads }.should_not raise_error
+    end
+
+    it "should return array of lines" do
+      Git.branch_heads.should == %w[ some popular text ]
+    end
+  end
+
+
+  describe :repo_name do
+    # this spec written because I replaced `pwd` with Dir.pwd
+    it "Dir.pwd should be same as `pwd`.chomp" do
+      Dir.pwd.should == `pwd`.chomp
+    end
+
+    it "should return hooks.emailprefix if it's not empty" do
+      expected = "name of repo"
+      mock(Git).from_shell("git config hooks.emailprefix") { expected }
+      dont_allow(Dir).pwd
+      Git.repo_name.should == expected
+    end
+
+    it "should return folder name if no emailprefix and directory not ended with .git" do
+      mock(Git).from_shell("git config hooks.emailprefix") { " " }
+      mock(Dir).pwd { "/home/someuser/repositories/myrepo" }
+      Git.repo_name.should == "myrepo"
+    end
+
+    it "should return folder name without extension if no emailprefix and directory ended with .git" do
+      mock(Git).from_shell("git config hooks.emailprefix") { " " }
+      mock(Dir).pwd { "/home/someuser/repositories/myrepo.git" }
+      Git.repo_name.should == "myrepo"
+    end
+  end
+
+end
+
+__END__
+
+ vim: tabstop=2 expandtab shiftwidth=2
