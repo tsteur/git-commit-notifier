6d3194a5529e376d111d02edd9bdc848262466e7
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-04-04 13:47:01 +0400

    Move specs accordingly

--- spec/lib/git_commit_notifier/result_processor_spec.rb
+++ spec/lib/git_commit_notifier/result_processor_spec.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-require File.expand_path('../../spec_helper', __FILE__)
+require File.expand_path('../../../spec_helper', __FILE__)
 require 'git_commit_notifier'
 
 include GitCommitNotifier
