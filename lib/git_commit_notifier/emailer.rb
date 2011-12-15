4f902c2f55ef665712fdd3f7bf73c32f00fed82d
James Berry <james@jberry.us>
2011-12-15 10:17:18 -0800

    Correct a missing escape in regex

--- lib/git_commit_notifier/emailer.rb
+++ lib/git_commit_notifier/emailer.rb
@@ -226,7 +226,7 @@ class GitCommitNotifier::Emailer
 
   # A quick-and-dirty regexp for determining whether a string contains any
   # characters that need escaping.
-  CHARS_NEEDING_QUOTING = /[^\x0a\x0d\x20-x7e]/
+  CHARS_NEEDING_QUOTING = /[^\x0a\x0d\x20-\x7e]/
 
   # Quote the given text if it contains any "illegal" characters
   def quote_if_necessary(text, charset)
