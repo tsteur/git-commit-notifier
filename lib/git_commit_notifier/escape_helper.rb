e4d7a19528a7292f37b31a55c41c700df38d1d39
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2012-04-03 18:02:07 +0400

    Actually two comment lines here.

--- lib/git_commit_notifier/escape_helper.rb
+++ lib/git_commit_notifier/escape_helper.rb
@@ -1,4 +1,5 @@
-# -*- coding: utf-8; mode: ruby; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- vim:fenc=utf-8:filetype=ruby:et:sw=2:ts=2:sts=2
+# -*- coding: utf-8; mode: ruby; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2
+# -*- vim:fenc=utf-8:filetype=ruby:et:sw=2:ts=2:sts=2
 
 # Provides content escaping helpers.
 module GitCommitNotifier::EscapeHelper
